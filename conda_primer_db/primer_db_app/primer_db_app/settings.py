7d43dd711dba478c7fd0f4792ef91cb48b773eb7
Ashley-Pritchard <ashley.pritchard@ouh.nhs.uk>
2020-07-29 13:45:06 +0000

    changed table style to allow break-words

--- conda_primer_db/primer_db_app/primer_db_app/settings.py
+++ conda_primer_db/primer_db_app/primer_db_app/settings.py
@@ -25,10 +25,10 @@ with open('/srv/Primer_DB_Key/secret_key.txt') as f:
 	SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', '']
 
 SITE_URL =""
 
