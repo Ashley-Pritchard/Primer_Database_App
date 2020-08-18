007654a4f05853ff3f014a84f0b392509f1cc3d6
Ashley-Pritchard <ashley.pritchard@ouh.nhs.uk>
2020-08-18 17:51:36 +0000

    refreshed migrations to fix bug of deleting primer names and changed admin page to show and search on primer name and location

--- conda_primer_db/primer_db_app/primer_db_site/admin.py
+++ conda_primer_db/primer_db_app/primer_db_site/admin.py
@@ -4,9 +4,19 @@ from primer_db_site.models import Amplicon, Analysis_Type, Gene, Primer_Set, Imp
 
 #add access to the database via the admin page 
 
-admin.site.register(Primer)
 admin.site.register(Amplicon)
 admin.site.register(Analysis_Type)
 admin.site.register(Gene)
 admin.site.register(Primer_Set)
 admin.site.register(Imported_By)
+
+class PrimerAdmin(admin.ModelAdmin):
+	list_display = ('name', 'location')
+	search_fields = ('name', 'location')
+
+	def active(self, obj):
+		return obj.is_active == 1
+
+	active.boolean = True
+
+admin.site.register(Primer, PrimerAdmin)
